2c0877de726ccb6d77883b7244b2a09ad43a2f0b
Aaron Zhao <aaron@Aarons-MacBook-Pro.local>
2017-02-10 18:45:34 +0000

    first commit

---
+++ run_v3.py
@@ -0,0 +1,29 @@
+import os
+import training_v3
+# os.system('python training_v3.py -p0')
+# os.system('python training_v3.py -p1')
+# os.system('python training_v3.py -p2')
+# os.system('python training_v3.py -p3')
+# os.system('python training_v3.py -p4')
+# os.system('python training_v3.py -p4')
+# os.system('python training_v3.py -p5')
+
+acc_list = []
+count = 0
+retrain = 0
+while (count < 10):
+    param = [
+        ('-p',count),
+        ('-tc1',0.12),
+        ('-tc2',0.14),
+        ('-tfc1',0.15),
+        ('-tfc2',0.15)]
+    acc = training_v3.main(param)
+    print (acc)
+    if (acc >= 0.99 or retrain >=3):
+        acc_list.append(acc)
+        retrain = 0
+        count = count + 1
+    else:
+        retrain += 1
+print('accuracy summary: {}'.format(acc_list))
