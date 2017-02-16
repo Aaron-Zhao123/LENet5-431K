173f1e1d96c1f1f38dad4b05df0c0aa7d45d742d
Aaron Zhao <aaron@dhcp-10-248-96-67.eduroam.wireless.private.cam.ac.uk>
2017-02-16 13:14:51 +0000

    fixed model number

--- run_v6.py
+++ run_v6.py
@@ -17,7 +17,7 @@ pfc2 = 93
 retrain = 4
 model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
 while (count < 2):
-    pfc = pfc+1
+    # pfc = pfc+1
     param = [
     ('-pcov',pcov),
     ('-pcov2',pcov2),
